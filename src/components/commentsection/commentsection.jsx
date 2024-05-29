f83fd2e0dc7e4bd103daf255615b46cc8478a365
SlavaD161 <96490726+SlavaD161@users.noreply.github.com>
2024-05-29 20:11:24 +0300

    Update commentsection.jsx

--- src/components/commentsection/commentsection.jsx
+++ src/components/commentsection/commentsection.jsx
@@ -1,12 +1,14 @@
+import React, { useEffect, useState } from 'react';
+import { collection, query, where, getDocs } from 'firebase/firestore';
+import { db } from '../../firebase/firebase';
 import CommentsList from "../commentlist/commentlist"
 import AddComment from "../addcomment/addcomment"
 
-
-const CommentSection = ({ animalId }) => {
+const CommentSection = ({ animalId, userId }) => { // Добавляем userId
     return (
         <div className="comment-section">
             <CommentsList animalId={animalId} />
-            <AddComment animalId={animalId} />
+            <AddComment animalId={animalId} userId={userId} /> {/* Передаем userId */}
         </div>
     );
 };
