4f2637cde06fa2e6c23a4e5017843eb6e6d45f8c
SlavaD161 <96490726+SlavaD161@users.noreply.github.com>
2024-05-29 18:22:47 +0300

    Create commentlist.jsx

---
+++ src/components/commentlist/commentlist.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import { collection, query, where, getDocs } from 'firebase/firestore';
+import { db } from '../../../firebase/firebase';
+
+const CommentsList = ({ animalId }) => {
+    const [comments, setComments] = useState([]);
+
+    useEffect(() => {
+        const fetchComments = async () => {
+            const q = query(collection(db, 'comments'), where('animalId', '==', animalId));
+            const querySnapshot = await getDocs(q);
+            const commentsData = querySnapshot.docs.map(doc => doc.data());
+            setComments(commentsData);
+        };
+
+        fetchComments();
+    }, [animalId]);
+
+    return (
+        <div className="comments-list">
+            <h3>Комментарии</h3>
+            {comments.map((comment, index) => (
+                <div key={index} className="comment">
+                    <p><strong>{comment.userName}:</strong> {comment.text}</p>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default CommentsList;
