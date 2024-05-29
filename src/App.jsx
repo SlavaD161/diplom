0c5771b6d6abf190457830588187b8b478f057a7
SlavaD161 <96490726+SlavaD161@users.noreply.github.com>
2024-05-29 19:03:10 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Categories from "./pages/categories/categories"
 import Animals from "./pages/animals/animals"
 import Animal from "./pages/animal/animal"
 import Adminpanel from "./pages/adminpanel/adminpanel"
+import CommentsList from "./components/commentlist/commentlist"; 
+import AddComment from "./components/addcomment/addcomment";
 
 const App = () => {
     return (
@@ -16,6 +18,8 @@ const App = () => {
                 <Route path="/animals" element={<Animals/>} />
                 <Route path="/animal" element={<Animal/>} />
                 <Route path="/animalexp_adminpanel" element={<Adminpanel/>} />
+                <Route path="/comments" element={<CommentsList/>} />
+                <Route path="/addcomment" element={<AddComment/>} />
             </Routes>
         </div>
     )
