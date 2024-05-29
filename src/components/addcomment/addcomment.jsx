53c4635735eb599466fa87841eb8504680cdb48c
SlavaD161 <96490726+SlavaD161@users.noreply.github.com>
2024-05-29 23:30:04 +0300

    Update addcomment.jsx

--- src/components/addcomment/addcomment.jsx
+++ src/components/addcomment/addcomment.jsx
@@ -1,17 +1,19 @@
 import React, { useState } from 'react';
-import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
+import { addDoc, collection } from 'firebase/firestore';
 import { db } from '../../firebase/firebase';
+import Button from "../ui/button/button"
 
 const AddComment = ({ animalId, userId, updateComments }) => {
     const [newComment, setNewComment] = useState('');
 
     const handleAddComment = async () => {
         if (newComment.trim()) {
+            const timestamp = new Date().toString(); // Преобразуем текущее время в строку
             await addDoc(collection(db, 'comments'), {
                 animalId: animalId,
                 userId: userId,
                 text: newComment,
-                timestamp: serverTimestamp(),
+                timestamp: timestamp, // Используем строку в качестве времени комментария
             });
             setNewComment('');
             updateComments(); // Обновляем список комментариев после добавления нового комментария
@@ -19,14 +21,26 @@ const AddComment = ({ animalId, userId, updateComments }) => {
     };
 
     return (
-        <div className="comment-form mt-[40px]">
+        <div className="comment-form mt-[40px] flex flex-col items-center">
             <textarea
                 value={newComment}
                 onChange={e => setNewComment(e.target.value)}
                 placeholder="Добавьте комментарий..."
-            ></textarea>
-            <button onClick={handleAddComment}>Отправить</button>
+                className="w-full max-w-[600px] h-[150px] p-4 border border-gray-300 rounded"
+            >
+
+            </textarea>
+            <Button
+                onClick={handleAddComment}
+                
+                classNames="btn mt-4 px-[35px] text-[17px] border-0 flex items-center hover:-translate-x-0.5 gap-1.5 text-[#fff] font-normal"
+                bg="#2EBB77"
+                hoverBg="#3166AF"
+            >
+                Отправить
+            </Button>
         </div>
+
     );
 };
 
